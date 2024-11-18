afe7de868c79dd1a21c3a1db756474d7ebe9dd6a
sanyaxx <sanyag2004@gmail.com>
2024-11-18 14:48:45 +0800

    Add debugging logs to the checkIfGameOver method.
    Added System.out.println statements to the checkIfGameOver method to log game state transitions:
    - Log when checking game-over conditions.
    - Log when the user is destroyed (game over).
    - Log when the user has reached the
    
    kill target (level advancement).
    Improves debugging and tracking of game flow during runtime.

---
+++ src/main/java/com/example/demo/Level1.java
@@ -0,0 +1,3 @@
+		System.out.println("Checking game over conditions...");
+			System.out.println("User has been destroyed. Game over.");
+			System.out.println("User  has reached kill target. Advancing to next level.");
