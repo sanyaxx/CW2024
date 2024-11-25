c9ba78783d862cc6abf284aaa770b5a8f2592dcd
sanyaxx <sanyag2004@gmail.com>
2024-11-26 03:55:19 +0800

    Create StartButton Class for Level Transition Handling in Game
    - Introduced the StartButton class to encapsulate the functionality of the start game button.
    - The class creates a button with a custom play image and sets its graphical properties for better visual integration.
    - Integrated LevelManager to manage level transitions, allowing the button to determine the next level and initiate the corresponding start screen.
    - Implemented the handleStartButtonClick method to handle button click events, retrieving the next level number and invoking the appropriate transition logic.
    - The button's background is set to transparent to maintain the desired aesthetic of the game interface.
    
    This addition enhances the game's user interface by providing a dedicated component for starting levels, improving code organization and maintainability by separating button logic from other game components.

---
+++ src/main/java/com/example/demo/StartButton.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.Objects;
+
+public class StartButton {
+    private final Button button;
+    private LevelManager levelManager;
+
+    public StartButton() {
+        button = new Button();
+        Image playImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/example/demo/images/startGameButton.png")));
+        ImageView playImageView = new ImageView(playImage);
+        playImageView.setFitWidth(250);
+        playImageView.setFitHeight(120);
+        playImageView.setPreserveRatio(true);
+        levelManager = LevelManager.getInstance();
+
+        button.setGraphic(playImageView);
+        button.setStyle("-fx-background-color: transparent;"); // Make the button background transparent
+    }
+
+    public Button getButton() {
+        return button; // Return the button instance
+    }
+
+    public void setOnAction(EventHandler<ActionEvent> eventHandler) {
+        button.setOnAction(eventHandler); // Set the action for the button
+    }
+
+    // New method to handle the start button click
+    public void handleStartButtonClick() {
+        int nextLevelNumber = levelManager.getNextLevelNumber();
+        System.out.println("nextLevelNumber: " + nextLevelNumber);
+        if (nextLevelNumber != -1) {
+            levelManager.showLevelStartScreen(nextLevelNumber); // Call goToNextLevel with the next level name
+        } else {
+            System.out.println("No more levels available.");
+        }
+    }
+}
