f590456359b622d58047dea639d3681ec0c4af87
sanyaxx <sanyag2004@gmail.com>
2024-11-28 14:16:04 +0800

    refactor: Change Boss Y_POSITION_UPPER_BOUND
    - Prevents Boss from moving above the display screen dimensions for better user experience.

--- src/main/java/com/example/demo/Boss.java
+++ src/main/java/com/example/demo/Boss.java
@@ -15,7 +15,7 @@ public class Boss extends FighterPlane {
 	private static final int MOVE_FREQUENCY_PER_CYCLE = 5;
 	private static final int ZERO = 0;
 	private static final int MAX_FRAMES_WITH_SAME_MOVE = 10;
-	private static final int Y_POSITION_UPPER_BOUND = -100;
+	private static final int Y_POSITION_UPPER_BOUND = 50;
 	private static final int Y_POSITION_LOWER_BOUND = 475;
 	private static final int MIN_SHIELD_DURATION = 100;
 	private static final int MAX_SHIELD_DURATION = 250;
